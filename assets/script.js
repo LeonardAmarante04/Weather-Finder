93db43ed28c94d91875dec963a207f1b6f8a2db4
Leonard Amarante <leonardamarante32@gmail.com>
2025-02-04 23:40:51 -0500

    added autocomplete

--- assets/script.js
+++ assets/script.js
@@ -1,12 +1,94 @@
+// Api keys
 const encryptedKey1 = btoa(`2294cc450dbd115f9a7714ec9ec6a4cd`);
 const weather_api_key = atob(encryptedKey1);
+const APIusername = "LeonardA04";
+const encryptedKey2 = btoa(`c68313b38a284dbd97bc3eace0c05ca4`);
+const geonamesAPi = atob(encryptedKey2);
 
-// global variables for storing weather information and styles
-let weatherData, forecastWeatherData, weatherDiv_created = false, weatherDate,
-    weathertemp, maxMin_temp, iconImg, srt, weatherDescription, cityHours, sunDownHour, sunUpHour, backgroundColor, backgroundImage, weatherdivHolders;
-
-// variable targeting a submit button
+// DOOM ELEMENTS
+const userInput = document.getElementById("userInput");
+const autocompleteHolder = document.getElementById("autocompleteHolder");
+const lookingText = document.getElementById("lookingText");
 const submitInput = document.getElementById("submitInput");
+const mainWeatherHolder = document.getElementById("mainWeatherHolder");
+const daysWeather = document.getElementById("daysWeather");
+const infoVisibility = document.getElementById("infoVisibility");
+// global variables for storing weather information and styles
+
+let weatherData, forecastWeatherData, weatherDiv_created = false, weatherDate,
+    weathertemp, maxMin_temp, iconImg, srt, weatherDescription, cityHours,
+    sunDownHour, sunUpHour, backgroundColor, backgroundImage, weatherdivHolders,
+    cityChoicesButton, chosecity, geosaveData, autocompleteData, buttonPressed = false;
+
+
+
+// auto completer!
+userInput.addEventListener("input", function () {
+    userinputVal = userInput.value
+
+
+    if (userinputVal.length >= 2) {
+
+        async function geonames() {
+
+
+            const geonameResponse = await fetch(`https://api.geoapify.com/v1/geocode/autocomplete?text=${userinputVal}&apiKey=${geonamesAPi}`);
+            geoData = await geonameResponse.json();
+            autocompleteData = geoData;
+            console.log(autocompleteData)
+            for (let i = 0; i < autocompleteData.features.length; i++) {
+
+
+                document.getElementById(`autocompleter${i}`).innerText = `${autocompleteData.features[i].properties.formatted}`
+                document.getElementById("autocompleteHolder").style.display = "block";
+                document.getElementById("lookingText").style.display = "none";
+
+
+                const buttons = document.querySelectorAll('button'); // Selects all button elements
+
+                buttons.forEach(button => {
+                    if (button.textContent === 'undefined') {
+                        button.textContent = '';
+                    }
+                });
+
+
+                document.getElementById(`autocompleter${i}`).addEventListener("click", function () {
+                    userInput.value = document.getElementById(`autocompleter${i}`).innerText;
+                    buttonPressed = true
+                    setTimeout(() => {
+                        buttonPressed = false;
+                    }, 2000);
+                    lat = autocompleteData.features[i].properties.lat
+                    lon = autocompleteData.features[i].properties.lon
+
+
+                });
+
+
+            }
+            if (!buttonPressed) {
+                lat = autocompleteData.features[0].properties.lat
+                lon = autocompleteData.features[0].properties.lon
+            }
+
+        };
+
+        geonames()
+
+    }
+
+
+});
+
+
+
+document.querySelector('body').addEventListener('click', function () {
+    document.getElementById("autocompleteHolder").style.display = "none";
+    document.getElementById("lookingText").style.display = "block";
+});
+
+
 
 
 // click event listener for submit button
@@ -14,7 +96,7 @@ submitInput.addEventListener("click", function () {
 
 
     // temperature unit conditions & storage values into variables
-    const userCity = document.getElementById("userInput").value
+    const userCity = userInput.value
     const fUnit = document.getElementById("f-unit");
     const cUnit = document.getElementById("c-unit");
     let unit;
@@ -51,29 +133,18 @@ submitInput.addEventListener("click", function () {
 
             try {
 
-                // Geo API DATA FETCH & MANIPULATION
-                const geoResponse = await fetch(`https://api.openweathermap.org/geo/1.0/direct?q=${userCity}&limit=5&appid=${weather_api_key}`);
-                const geoData = await geoResponse.json();
-
-                // condition for if geoData.legnt is equal to 0 than storage error city not found
-                if (geoData.length === 0) {
-                    throw new Error("City not found");
-                }
-
-
-
-                const { lat, lon } = geoData[0];
-
 
                 // Geo API DATA FETCH & MANIPULATION
                 const weatherResponse = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${weather_api_key}&units=${unit}`);
                 weatherData = await weatherResponse.json();
                 weatherDescription = weatherData.weather[0].main;
+
+
                 // gets sunset data from weatherData API
                 const sundownUnix = weatherData.sys.sunset
-                 // gets sunrise data from weatherData API
+                // gets sunrise data from weatherData API
                 const sunupUnix = weatherData.sys.sunrise
-             
+
                 // five day weather forecast API DATA FETCH & MANIPULATION
                 const forecastWeatherResponse = await fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${weather_api_key}&units=${unit}`);
                 forecastWeatherData = await forecastWeatherResponse.json()
@@ -81,7 +152,8 @@ submitInput.addEventListener("click", function () {
 
 
                 // timezone API DATA FETCH & MANIPULATION
-                const timezoneResponse = await fetch(`https://secure.geonames.org/timezoneJSON?lat=${lat}&lng=${lon}&username=LeonardA04`);
+                const timezoneResponse = await fetch(`https://secure.geonames.org/timezoneJSON?lat=${lat}&lng=${lon}&username=${APIusername}`);
+
                 const timezoneData = await timezoneResponse.json();
                 // gets the date and time data of timezone API
                 const cityTime = timezoneData.time;
@@ -89,7 +161,7 @@ submitInput.addEventListener("click", function () {
                 cityHours = cityTime.substring(11, 13);
                 // converts the sunset unix value into a readable format 
                 const sundownTime = new Date(sundownUnix * 1000).toLocaleTimeString("en-GB", { timeZone: `${timezoneData.timezoneId}` })
-                  // converts the sunrise unix value into a readable format 
+                // converts the sunrise unix value into a readable format 
                 const sunupTime = new Date(sunupUnix * 1000).toLocaleTimeString("en-GB", { timeZone: `${timezoneData.timezoneId}` })
                 // filters sunDownHour string value between a start value and a end value which is the new value of the sunDownHour variable
                 sunDownHour = sundownTime.substring(0, 2);
@@ -107,7 +179,7 @@ submitInput.addEventListener("click", function () {
                     backgroundColor = `rgb(11,11,11)`;
                     backgroundImage = `url(./assets/nightStars.jpeg)`;
 
-                    
+
                     const weatherMessages = {
                         "Clouds": `./assets/weatherVideos/nightClouds.mp4`,
                         "Clear": `./assets/weatherVideos/clearNight.mp4`,
@@ -170,6 +242,8 @@ submitInput.addEventListener("click", function () {
                 const mainIcon = document.createElement("img");
                 // icon weather for forecast div
                 mainIcon.src = `http://openweathermap.org/img/wn/${weatherData.weather[0].icon}.png`;
+
+
                 document.getElementById("cityName").textContent = weatherData.name;
                 document.getElementById("cityName").appendChild(mainIcon)
                 document.getElementById("temperature").textContent = `Temperature: ${Math.floor(weatherData.main.temp)}${unitSymbol}`
@@ -209,10 +283,6 @@ submitInput.addEventListener("click", function () {
                         // appending the weatherdivholders
                         document.getElementById("daysWeather").appendChild(weatherdivHolders)
 
-
-
-
-
                         // creates text elements for the use of displaying the 5 days boardcast
                         weatherdivHolders.id = `weatherDivHolder${i}`;
                         weatherDate = document.createElement("h3");
@@ -232,16 +302,13 @@ submitInput.addEventListener("click", function () {
                         weathertemp.id = `weatherTemp${i}`;
                         maxMin_temp.id = `maxMinTemp${i}`;
 
-
-
-
                         // adds the text content of the text elements that shows the api information of the weather during 5 days
                         iconImg.src = `http://openweathermap.org/img/wn/${fiveDayWeather[i].weather[0].icon}.png`;
-                        srt = fiveDayWeather[i].dt_txt
+                        srt = fiveDayWeather[i].dt_txt;
                         weatherDate.textContent = srt.substring(0, 10);
                         weatherDate.appendChild(iconImg);
                         weathertemp.textContent = `Temperature: ${Math.floor(fiveDayWeather[i].main.temp)}${unitSymbol}`;
-                        maxMin_temp.textContent = `Min: ${Math.floor(fiveDayWeather[i].main.temp_min)}${unitSymbol} Max: ${Math.floor(fiveDayWeather[i].main.temp_max)}${unitSymbol}`;
+                        maxMin_temp.textContent = `Wind Speed: ${fiveDayWeather[i].wind.speed}`;
                         weatherdivHolders.append(weatherDate, weathertemp, maxMin_temp);
 
 
@@ -255,7 +322,7 @@ submitInput.addEventListener("click", function () {
 
                     // if divs are created than just change the contenct of the elements already created  
                     for (let q = 0; q < 5; q++) {
-                        
+
                         document.getElementById(`weatherDivHolder${q}`).style.backgroundColor = backgroundColor;
                         document.getElementById(`weatherDivHolder${q}`).style.backgroundImage = backgroundImage;
                         document.getElementById(`weatherDivHolder${q}`).style.border = `solid 2px ${backgroundColor}`;
@@ -265,7 +332,7 @@ submitInput.addEventListener("click", function () {
                         document.getElementById(`weatherDate${q}`).textContent = srt.substring(0, 10);
                         document.getElementById(`weatherDate${q}`).appendChild(iconImg);
                         document.getElementById(`weatherTemp${q}`).textContent = `Temperature: ${Math.floor(fiveDayWeather[q].main.temp)}${unitSymbol}`;
-                        document.getElementById(`maxMinTemp${q}`).textContent = `Min: ${Math.floor(fiveDayWeather[q].main.temp_min)}${unitSymbol} Max: ${Math.floor(fiveDayWeather[q].main.temp_max)}${unitSymbol}`;
+                        document.getElementById(`maxMinTemp${q}`).textContent = `Wind Speed: ${fiveDayWeather[q].wind.speed}`;
 
 
                     }
@@ -282,12 +349,10 @@ submitInput.addEventListener("click", function () {
             }
         }
 
-
         fetchWeatherdata()
     }
 
 
 
 });
-
 
